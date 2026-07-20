9be037cd1c505d6952ec6eb5f7f50e13fc61efed
Nicolas G <nicolas34130@gmail.com>
2026-07-20 19:36:21 +0200

    Skip Livewire blocks with dynamic component names instead of throwing
    @livewire($tile['view'], [...]) compiles to $__split($tile['view'], ...):
    the args-extraction regex only matches literal component names, so these
    blocks hit the ShouldNotHappenException and abort the analysis of the whole
    file. A dynamic component cannot be statically resolved anyway - skip the
    block and keep analysing the rest of the template.

---
+++ tests/Compiler/BladeToPHPCompiler/Fixture/livewire_dynamic_component.blade.php
@@ -0,0 +1,8 @@
+@livewire($dynamicName, ['b' => $b])
+-----
+<?php
+
+/** @var Illuminate\Support\ViewErrorBag $errors */
+/** @var Illuminate\View\Factory $__env */
+/** @var Illuminate\Foundation\Application $app */
+/** file: foo.blade.php, line: 1 */
